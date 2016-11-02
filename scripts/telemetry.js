d793b97273d8529553d08d940a31f13641599e6b
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-02 13:47:09 -0600

    bootstrap method

--- scripts/telemetry.js
+++ scripts/telemetry.js
@@ -4,8 +4,9 @@
 
     var getDOMPath = function(domPath) {
         // traverse the array in reverse
-        for(var i = domPath.length - 1; i >= 0; i--) {
-        }
+        // for(var i = domPath.length - 1; i >= 0; i--) {
+        // }
+        return '';
     };
 
     var getTimestamp = function() {
@@ -29,9 +30,26 @@
             //Flush the stack
             stack = [];
         }
+        console.log(evt);
     };
 
-    document.onclick = handleEvent;
-    document.onkeypress = handleEvent;
+    var bootstrapTelemetry = function() {
+        // Get all elements with telemetry attribute
+        var telemetryElements = document.querySelectorAll('[data-telemetry]');
+        if(elements.length === 0) {
+            return;
+        }
+        // For element, attach an event handler
+        telemetryElements.forEach(function(element) {
+            element.onclick = handleEvent;
+            // if input element, attach a keypress event
+            if(element.tagName === 'input') {
+                element.keypress = handleEvent;
+            }
+        });
+    };
+
+    // document.onclick = handleEvent;
+    // document.onkeypress = handleEvent;
 
 })();
