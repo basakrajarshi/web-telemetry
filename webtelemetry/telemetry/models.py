61e4839eea01cada3ec381784ab97a4a5cfe19ec
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-03 11:36:08 -0600

    add migration and models

--- webtelemetry/telemetry/models.py
+++ webtelemetry/telemetry/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+EVENT_CHOICES = (
+    ('CLICK', 'click'),
+    ('KEYPRESS', 'keypress')
+)
+
+class TelemetryItem(models.Model):
+    event_type = models.CharField(choices=EVENT_CHOICES, blank=False, max_length=10)
+    os = models.CharField(max_length=30, blank=False)
+    user_agent = models.CharField(max_length=150, blank=False)
+    timestamp = models.DateField(blank=False)
