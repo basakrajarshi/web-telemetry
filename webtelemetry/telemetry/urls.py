06d69c49ccd3f9f889d879f376709c0eb0a8708a
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-08 12:59:27 -0700

    fix faulty url mapping

--- webtelemetry/telemetry/urls.py
+++ webtelemetry/telemetry/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from views import insert_telemetry_data_view
+from views import insert_telemetry_data_view, check_session
 
 urlpatterns = [
     url(r'^insert/', insert_telemetry_data_view),
